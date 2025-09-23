b49e3088ff0a3fbfd3973affbfd3e7798c15d08d
Ravi Basil <142879212+RaviBytesMagic@users.noreply.github.com>
2025-09-23 08:30:46 +0530

    added build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 
 import DepthCarousal from "./DepthCarousal";
+import LeftAlignedCarousal from "./DepthCarousal/LeftAlignedCarousal";
+import DiagonalCarousel from "./DiagonalCarousal";
 
 function App() {
   return (
     <div className="App">
+      <LeftAlignedCarousal />
       <DepthCarousal />
-      {/* <DiagonalCarousel /> */}
+      <DiagonalCarousel />
     </div>
   );
 }
